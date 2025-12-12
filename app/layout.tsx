7b9e688e4623a1099004d9c3f11fca3e13c1464b
Simon Risk <simonnrisk@gmail.com>
2025-12-12 09:47:57 -0500

    fix: revert webchat version to v3.3 to avoid updateUser bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,8 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <script src="https://cdn.botpress.cloud/webchat/v3.5/inject.js"></script>
+        {/* Must use v3.3 for updateUser to work */}
+        <script src="https://cdn.botpress.cloud/webchat/v3.3/inject.js"></script>
         <script src="https://files.bpcontent.cloud/2025/12/09/21/20251209214238-BRGDOS1V.js" defer></script>
       </body>
     </html>
