67b0ad15d14fe7cf98da682c528cad64d5c32c75
abhiramshaji-cyber <abhiram.shaji@botpress.com>
2026-01-07 12:26:11 -0500

    feat : update client workspace

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,10 @@ export default function RootLayout({
       <body>
         {children}
         {/* Must use v3.3 for updateUser to work */}
-        <script src="https://cdn.botpress.cloud/webchat/v3.3/inject.js"></script>
-        <script src="https://files.bpcontent.cloud/2025/12/09/21/20251209214238-BRGDOS1V.js" defer></script>
+
+        <script src="https://cdn.botpress.cloud/webchat/v3.5/inject.js"></script>
+        <script src="https://files.bpcontent.cloud/2026/01/07/15/20260107150430-TIG10B51.js" defer></script>
+
       </body>
     </html>
   )
